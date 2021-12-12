07379583feb74f656c3c4a725ef4860247834b1f
Colin Trezise <ctrezise@asu.edu>
2021-12-12 14:52:05 -0700

    committing before major revision

--- ppm/ppm.hs
+++ ppm/ppm.hs
@@ -14,10 +14,25 @@ main = do
         pixelCount = rows * columns
     writeFile file header
     gen <- getStdGen
-    let image = B.pack $ take (3 * (rows * columns)) $ clownVomit gen channelMax
+    let image = B.concat $ map pixelBytes $ take pixelCount $ purpleStatic gen
+    --let image = B.pack $ take (3 * (rows * columns)) $ clownVomit gen channelMax
     B.appendFile file image
+
+data Pixel = Pixel { red :: Word8, green :: Word8, blue :: Word8 } deriving Show
+
+pixelBytes :: Pixel -> B.ByteString
+pixelBytes p = let pixels = [red p, green p, blue p]
+               in  B.pack pixels
 
 channelMax = 255
 
+randomBytes :: StdGen -> [Word8]
+randomBytes gen = randomRs (10,channelMax) gen
+
+purpleStatic :: StdGen -> [Pixel]
+purpleStatic gen = map (\w -> Pixel w 0 w) $ randomBytes gen
+
 clownVomit :: StdGen -> Word8 -> [Word8]
 clownVomit gen chnlMax = randomRs (0,chnlMax) gen
+
+
